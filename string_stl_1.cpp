3e4cefe15d3dcab4da8a00cdf259d00366669b0a
Uttam Anand <54016758+uttam-anand@users.noreply.github.com>
2020-07-31 18:46:04 +0530

    Add files via upload

---
+++ string_stl_1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	string s1="hello";
+	string s2("hello");
+	string s3=string("hello");
+	string s4=s1+s2;
+	cout<<s1<<endl<<s2<<endl<<s3<<endl<<s4;
+	
+}
