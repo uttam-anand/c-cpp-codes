0e55ede6464846e72a51d9a21eca5b7d9a06e005
Uttam Anand <54016758+uttam-anand@users.noreply.github.com>
2020-07-31 18:46:33 +0530

    Add files via upload

---
+++ jotc.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	int n,j,c,e=100,pos=0;
+	cin>>n>>j;
+    vector<int>v;
+    for(int i=0;i<n;i++)
+    {
+    	cin>>c;
+    	v.insert(v.begin()+i,c);
+    }
+    while(1)
+    {
+    	pos=(pos+j)%n;
+    	if(v[pos]==1)
+    		e-=3;
+    	else
+    		e-=1;
+    	if(pos==0)
+    		break;
+    }
+    cout<<e;
+}
