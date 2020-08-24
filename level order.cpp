bb36f7d78e101a8af8db685ee596d1f6adae264f
Uttam Anand <54016758+uttam-anand@users.noreply.github.com>
2020-08-24 19:33:35 +0530

    Create level order.cpp

---
+++ level order.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>	
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+void levelOrder(Node * root)
+{
+if (root == NULL)  return; 
+  
+    // Create an empty queue for level order traversal 
+    queue<Node *> q; 
+  
+    // Enqueue Root and initialize height 
+    q.push(root); 
+  
+    while (q.empty() == false) 
+    { 
+        // Print front of queue and remove it from queue 
+        Node *node = q.front(); 
+        cout << node->data << " "; 
+        q.pop(); 
+  
+        /* Enqueue left child */
+        if (node->left != NULL) 
+            q.push(node->left); 
+  
+        /*Enqueue right child */
+        if (node->right != NULL) 
+            q.push(node->right); 
+    } 
+}
+
+}; //End of Solution
