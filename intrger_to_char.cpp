4f79f169e9e530dcd6a4ca7ba2fcd27cc0dddccf
Uttam Anand <54016758+uttam-anand@users.noreply.github.com>
2020-10-18 13:00:34 +0530

    Add files via upload

---
+++ intrger_to_char.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif	
+	int n=9035983;
+	string str;
+	/*stringstream ss;
+	ss<<n;
+	ss>>str;
+	cout<<str;*/
+	str=to_string(n);
+	cout<<str;
+	return 0;
+}
